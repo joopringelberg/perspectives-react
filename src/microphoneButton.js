296001302042652e31f39a1070907fad69469a8c
Joop Ringelberg <joop.ringelberg@gmail.com>
2024-10-30 16:12:37 +0100

    Fixed ref forwarding in microphone button

--- src/microphoneButton.js
+++ src/microphoneButton.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { forwardRef } from 'react';
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import Button from "./button.js";
@@ -6,19 +7,27 @@ import Button from "./button.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMicrophone } from "@fortawesome/free-solid-svg-icons/faMicrophone";
 
-export const MicrophoneButton = ({ className = "", children, ...rest }) => {
-  const cName = `cs-button--voicecall`;
+// export const MicrophoneButton = forwardRef( ({ className = "", children, ...rest }) => {
+export const MicrophoneButton = forwardRef( (props, ref) => {
+    const cName = `cs-button--voicecall`;
+    const rest = {};
+    Object.keys(props).forEach( key => {
+      if (!(key == "children" || key == "className"))
+      {
+        rest[key] = props[key];
+      }
+    })
 
   return (
     <Button
       {...rest}
-      className={classNames(cName, className)}
+      className={classNames(cName, props.className ? props.className : "")}
       icon={<FontAwesomeIcon icon={faMicrophone} />}
     >
-      {children}
+      {props.children}
     </Button>
   );
-};
+});
 
 MicrophoneButton.propTypes = {
   /** Primary content. */

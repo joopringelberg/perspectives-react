1b32d39da269eed3fde39ac5d4009f9b3587fcab
Joop Ringelberg <joop.ringelberg@gmail.com>
2024-12-18 10:17:21 +0100

    Webpack now includes source map files. I did not succeed in putting the correct source map file name in the generated maps. However, all of perspectives-react's sources are available in the MyContexts/src directory in Chrome webtools. This is a fortuitous coincidence as the generated source map name in perspectives-react is "index.js.map", while the entry point in MyContexts is also "index.js".

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,20 @@ const path = require("path");
 const webpack = require('webpack');
 
 module.exports = {
-  entry: path.join(__dirname, "src/perspectives-react-components.js" ),
+  entry: path.join(__dirname, "src/perspectives-react-components.js"),
   output: {
     library: "perspectives-react",
-    // Type "umd" seems attractive but will break the system for react-bootstrap!
-    // Desalniettemin is het nu een optie die werkt voor InPlace.
     libraryTarget: "umd",
     filename: "perspectives-react.js",
-    path: path.join(__dirname, "dist")
+    path: path.join(__dirname, "dist"),
+    sourceMapFilename: "perspectives-react.js.map" // Ensure this line is included
   },
   mode: "development",
   target: "web",
+  devtool: 'source-map', // Ensure this line is included
   module: {
-    rules: [{
+    rules: [
+      {
         test: /.jsx?$/,
         exclude: /node_modules/,
         use: [
@@ -23,27 +24,27 @@ module.exports = {
             options: {
               presets: ['@babel/preset-env', "@babel/preset-react"],
               plugins: [
-                // '@babel/plugin-proposal-object-rest-spread',
                 '@babel/plugin-syntax-dynamic-import'
-              ]
+              ],
+              sourceMaps: true // Ensure this line is included
             }
           }
         ]
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      }]
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
       __PPSTORAGELIMIT__: 10,
-      __PPWARNINGLEVEL__: 5, 
+      __PPWARNINGLEVEL__: 5,
       __PPSTORAGEURL__: '"https://mycontexts.com/ppsfs/uploadfile"'
-    })        
+    })
   ],
-  externals:
-    {
+  externals: {
     "react": {
       amd: "react",
       commonjs: "react",
@@ -67,21 +68,21 @@ module.exports = {
       commonjs: "@primer/octicons-react",
       commonjs2: "@primer/octicons-react",
       root: "@primer/octicons-react"
-    }
-    ,"prop-types": {
-       amd: "prop-types",
-       commonjs: "prop-types",
-       commonjs2: "prop-types",
-       root: "prop-types"
-     }
-    ,"perspectives-proxy": {
+    },
+    "prop-types": {
+      amd: "prop-types",
+      commonjs: "prop-types",
+      commonjs2: "prop-types",
+      root: "prop-types"
+    },
+    "perspectives-proxy": {
       amd: "perspectives-proxy",
       commonjs: "perspectives-proxy",
       commonjs2: "perspectives-proxy",
       root: "perspectives-proxy"
-    }
-    , "pouchdb-browser": "commonjs pouchdb-browser",
-     "i18next": {
+    },
+    "pouchdb-browser": "commonjs pouchdb-browser",
+    "i18next": {
       amd: "i18next",
       commonjs: "i18next",
       commonjs2: "i18next",
